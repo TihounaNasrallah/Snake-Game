d1b06bf415e5fe968ca36dac759f9cf07863f42f
Ti7ouna <tihounanasrallah@gmail.com>
2023-01-11 23:17:02 +0100

    test

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 120)
 
+#testinngggg
+
 #Functions
 def event_loop():
     global again, in_action, in_action_bis, nu_head_position, program
